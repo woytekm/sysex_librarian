e6cb3dd0c4849088307f83c22f350fd5b1e727e6
woytekm <woytekm@gmail.com>
2016-07-26 22:07:33 +0000

    initial commit

--- midi_out.c
+++ midi_out.c
@@ -4,7 +4,7 @@
 uint8_t MIDI_write_sysex_buffer(unsigned char *sysex_buffer, uint32_t len)
  {
 
-   #define SYSEX_CHUNK 16
+   #define SYSEX_CHUNK 8
 
    uint8_t buffer_empty;
    uint32_t offset = 0;
@@ -13,18 +13,6 @@ uint8_t MIDI_write_sysex_buffer(unsigned char *sysex_buffer, uint32_t len)
    int MIDI_fd;
    mode_t mode;
    
-   char *test = "sdfsdfdsf";
-
-   mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-
-   MIDI_fd = open("sysex_dump_compare.bin",O_WRONLY|O_CREAT|O_TRUNC,mode);
-
-   if(MIDI_fd == -1)
-    {
-     SYS_debug(DEBUG_NORMAL,"SYS: open file failed, errno %d",errno);
-     return;
-    }
-
    chunks = len/SYSEX_CHUNK;
    partial_chunk = len%SYSEX_CHUNK;
 
@@ -33,27 +21,23 @@ uint8_t MIDI_write_sysex_buffer(unsigned char *sysex_buffer, uint32_t len)
 
    if((chunks == 0) && (partial_chunk > 0))
     {
-     write(MIDI_fd,sysex_buffer+offset,partial_chunk);
-     close(MIDI_fd);
+     write(G_MIDI_fd,sysex_buffer+offset,partial_chunk);
      return 1;
     }
 
    for(i=1; i<=chunks; i++)
     {
-     write(MIDI_fd,sysex_buffer+offset,SYSEX_CHUNK);
+     write(G_MIDI_fd,sysex_buffer+offset,SYSEX_CHUNK);
      offset += SYSEX_CHUNK;
-     usleep(5000);
+     usleep(10000);
      SYS_debug(DEBUG_NORMAL,"SYS: wrote chunk %d, (%d bytes)",i,offset);
     }
 
    if(partial_chunk > 0)
     {
-     write(MIDI_fd,sysex_buffer+offset,partial_chunk);
+     write(G_MIDI_fd,sysex_buffer+offset,partial_chunk);
      SYS_debug(DEBUG_NORMAL,"SYS: wrote partial chunk %d, (%d bytes)",i,offset+partial_chunk);
     }
-
- 
-   close(MIDI_fd);
 
    return 1;
 
